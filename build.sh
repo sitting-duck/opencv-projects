75f5742d7bf391021eb960eed622139019d536b5
sitting-duck <ashley.tharp@gmail.com>
2022-07-15 16:09:36 -0500

    pushing init commit, can't remember which version of opencv I was building, looks like I was just building from the tip of master like a total noob lol

--- build.sh
+++ build.sh
@@ -8,24 +8,27 @@ git clone https://github.com/opencv/opencv_contrib.git
 # cleanup step
 rm -rf ./*/CMakeCache.txt
 
-# Regular Build - No Cuda Support
+# -DWITH_OPENNI2=ON tested and works for both debug and release
+
 mkdir -p build_release && mkdir -p build_debug
-cd build_release && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON ../opencv && make -j16
+cd build_release && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=ON ../opencv && make -j16
+sudo make install
 cd ..
-cd build_debug && cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_EXAMPLES=ON ../opencv && make -j16
+cd build_debug && cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_EXAMPLES=ON -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=ON ../opencv && make -j16
+sudo make install
 cd .. 
 
 # With Cuda Support
-mkdir -p build_release_cuda && mkdir -p build_debug_cuda
+#mkdir -p build_release_cuda && mkdir -p build_debug_cuda
 
 # turn on Cuda support
-sed -i '' 's/\"Include NVidia Cuda Runtime support\" OFF/\"Include NVidia Cuda Runtime support\" ON/g' $PWD/opencv/CMakeLists.txt
+#sed -i '' 's/\"Include NVidia Cuda Runtime support\" OFF/\"Include NVidia Cuda Runtime support\" ON/g' $PWD/opencv/CMakeLists.txt
 
-cd build_release_cuda && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON ../opencv && make -j16
-cd ..
-cd build_debug_cuda && cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON ../opencv && make -j16
-cd ..
+#cd build_release_cuda && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON ../opencv && make -j16
+#cd ..
+#cd build_debug_cuda && cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON ../opencv && make -j16
+#cd ..
 # turn back off Cuda Support to return CMakeLists.txt to original state
-sed -i '' 's/\"Include NVidia Cuda Runtime support\" ON/\"Include NVidia Cuda Runtime support\" OFF/g' $PWD/opencv/CMakeLists.txt
+#sed -i '' 's/\"Include NVidia Cuda Runtime support\" ON/\"Include NVidia Cuda Runtime support\" OFF/g' $PWD/opencv/CMakeLists.txt
 
 
