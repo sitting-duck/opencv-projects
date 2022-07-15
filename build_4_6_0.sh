13287b7c68269b257ffe46f996e92f94b89f11ca
sitting-duck <ashley.tharp@gmail.com>
2022-07-15 18:39:36 -0500

    add debug build and added opencv_contrib submodule

--- build_4_6_0.sh
+++ build_4_6_0.sh
@@ -1,6 +1,32 @@
 
+# use tags to checkout the version we want 
 cd opencv
 git fetch --all --tags
 git checkout tags/4.6.0 -b v4.6.0-build-branch
+cd ..
+
+# back inside project root we make all the build dirs
+prefix=build_4_6_0_
+release_dir=${prefix}_release
+debug_dir=${prefix}_debug
+
+# overwrite if they already exist with -p
+mkdir -p ${release_dir}
+mkdir -p ${debug_dir}
+
+# release build
+cd ${release_dir}
+cmake -D CMAKE_BUILD_TYPE=RELEASE -DBUILD_EXAMPLES=ON -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=ON ../opencv
+make
+cd ..
+
+# debug build
+cd ${debug_dir}
+cmake -D CMAKE_BUILD_TYPE=DEBUG -DBUILD_EXAMPLES=ON -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=ON ../opencv
+make
+cd ..
+
+
 
 # TBD ...
+
