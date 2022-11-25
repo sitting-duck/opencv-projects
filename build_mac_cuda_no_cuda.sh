4aa76e2862dd44f469dd043531ebfb0ad441cd97
Ashley <ashley.tharp@gmail.com>
2022-11-25 12:25:33 -0600

    Add files via upload

---
+++ build_mac_cuda_no_cuda.sh
@@ -0,0 +1,30 @@
+# Build Script MacOS 10.15 Catalina
+# 05/03/2021
+# Ashley Tharp - ashley.tharp@gmail.com
+
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+
+# cleanup step
+rm -rf ./*/CMakeCache.txt
+
+# Regular Build - No Cuda Support
+mkdir -p build_release && mkdir -p build_debug
+cd build_release && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON ../opencv && make -j16
+cd ..
+cd build_debug && cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_EXAMPLES=ON ../opencv && make -j16
+
+# With Cuda Support
+mkdir -p build_release_cuda && mkdir -p build_debug_cuda
+
+# turn on Cuda support
+sed -i '' 's/\"Include NVidia Cuda Runtime support\" OFF/\"Include NVidia Cuda Runtime support\" ON/g' ./opencv/CMakeLists.txt
+
+cd build_release && cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON ../opencv && make -j16
+cd ..
+cd build_debug && cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_EXAMPLES=ON -DWITH_CUDA=ON ../opencv && make -j16
+
+# turn back off Cuda Support to return CMakeLists.txt to original state
+sed -i '' 's/\"Include NVidia Cuda Runtime support\" ON/\"Include NVidia Cuda Runtime support\" OFF/g' ./opencv/CMakeLists.txt
+
+
